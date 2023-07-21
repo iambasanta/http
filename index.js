babaf50bdd695bfc7deb1512074a17c0a5d3d060
iambasanta <sangpangbasanta@gmail.com>
2023-07-21 21:37:49 +0545

    Maintain a log of requests

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const http = require("http");
 
 const PORT = 8000;
 const httpServer = http.createServer();
+const fs = require("fs");
 
 httpServer.on("connection", (connection) => {
   console.log("CONNECTED");
 });
 
 httpServer.on("request", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.writeHead(200);
-  res.end(
-    `<html><head><title>html</title></head><body><h1>HTML</h1></body></html>`
-  );
+  const userAgent = req.headers["user-agent"];
+  const log = `${Date.now()}: NEW REQUEST FROM: ${userAgent}\n`;
+  fs.appendFile("log.txt", log, (error, data) => {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.write(
+      `<html><head><title>html</title></head><body><h1>HTML</h1></body></html>`
+    );
+    res.end();
+  });
 });
 
 function countConnections() {

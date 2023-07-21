cca111ac05e5f89f2f613e236e3297760397208b
iambasanta <sangpangbasanta@gmail.com>
2023-07-21 20:59:45 +0545

    Count the number of connections

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ httpServer.on("connection", (connection) => {
   console.log("CONNECTED");
 });
 
+function countConnections() {
+  httpServer.getConnections((err, count) => {
+    console.log(`NO. OF CONNECTIONS: ${count}`);
+  });
+  setTimeout(countConnections, 3000);
+}
+
+countConnections();
+
 httpServer.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
